79e7dc829a456104464d4de330a405b751b8abce
amitkrmaharana <ak657harley@gmail.com>
2021-01-28 10:06:05 +0530

    Deleted files inherited from Sequence

--- adddice.sh
+++
@@ -1,9 +0,0 @@
-#Add two Random Dice Number and Print the Result
-
-
-
-#!/bin/bash -x
-dicenum1=$((RANDOM%6+1))
-dicenum2=$((RANDOM%6+1))
-dicesum=$((dicenum1+dicenum2))
-echo $dicesum
--- singleran.sh
+++
@@ -1,6 +0,0 @@
-#Use Random Function (( RANDOM )) to get Single Digit
-
-
-#!/bin/bash -x
-digit=$((RANDOM%10))
-echo $digit
