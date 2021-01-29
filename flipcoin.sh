bd5365b0e2adfe465cd5fecb35c6660425573101
amitkrmaharana <ak657harley@gmail.com>
2021-01-29 08:13:57 +0530

    Added flipcoin.sh

---
+++ flipcoin.sh
@@ -0,0 +1,12 @@
+#Write a program to simulate a coin flip and print out "Heads" or "Tails" accordingly.
+
+
+
+#!/bin/bash -x
+flip=$((RANDOM%2))
+if [ $flip -eq 0 ]
+then
+        echo HEADS
+else
+        echo TAILS
+fi
